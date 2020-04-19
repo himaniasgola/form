7159188a3f0f1c3a18edf7b7fa7a0a7d1a08e480
himaniasgola <62131249+himaniasgola@users.noreply.github.com>
2020-04-19 10:24:55 +0530

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,98 @@
+<template>
+    <div class="container">
+        <form v-if="!isSubmitted">
+            <div class="row">
+                <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+                    <h1>SignUp</h1>
+                    <hr>
+                    <div class="form-group">
+            <label for="firstName">First Name</label>
+            <input type="text" id="firstName" class="form-control" v-model="userData.firstName">
+          </div>
+          <div class="form-group">
+            <label for="lastName">Last Name</label>
+            <input type="text" id="lastName" class="form-control" v-model="userData.lastName">
+          </div>
+                    <div class="form-group">
+                        <label for="email">Mail</label>
+                        <input
+                                type="text"
+                                id="email"
+                                class="form-control"
+                                :value="userData.email"
+                                @input="userData.email= $event.target.value">
+                    </div>
+                    <div class="form-group">
+                        <label for="password">Password</label>
+                        <input
+                                type="password"
+                                id="password"
+                                class="form-control"
+                                :value="userData.password"
+                                @input="userData.password= $event.target.value">
+                    </div>
+                    <p>{{ userData.password }}</p>
+                    <div class="row">
+            <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3 form-group">
+              <label for="yes">
+                <input type="radio" id="yes" value="Yes" v-model="storeData"> Yes
+              </label>
+              <label for="no">
+                <input type="radio" id="no" value="No" v-model="storeData"> No
+              </label>
+            </div>
+          </div>
+          <div class="row">
+                <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+                    <button
+                            class="btn btn-primary" 
+                            @click.prevent="submitted">Submit!
+                    </button>
+                </div>
+            </div>
+                </div>
+            </div>
+        </form>
+        <hr>
+        <div class="row" v-if="isSubmitted">
+            <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+                <div class="panel panel-default">
+                    <div class="panel-heading">
+                        <h4>Your Data</h4>
+                    </div>
+                    <div class="panel-body">
+                        <p>Full Name: {{ userData.firstName }} {{ userData.lastName }}</p>
+                        <p>Mail: {{ userData.email }}</p>
+                        <p>Password:{{ userData.password }} </p>
+                        <p>Store in Database?: {{ storeData }}</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        data () {
+            return {
+                userData: {
+                    firstName: '',
+                    lastName: '',
+                    password: '',
+                    email: '',
+                },
+                storeData: 'Yes',
+                isSubmitted: false
+            }
+        },
+        methods: {
+            submitted() {
+                this.isSubmitted = true;
+            }
+        }
+    }
+</script>
+
+<style>
+</style>
